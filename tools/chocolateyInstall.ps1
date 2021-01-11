7480c30e713fb806389959c72ba7681bb1c390ba
Chris Blyth <blythmeister@gmail.com>
2021-01-11 13:10:03 +0000

    Add option to specify a path to be used for install

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -4,7 +4,16 @@ $checksum       = 'a55792e693d6833ff4d6b1b2efd94fb93c244c859e33c831699e3124408c5
 $checksumType   = 'sha256'
 $validExitCodes = @(0)
 
-$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$params = Get-PackageParameters
+
+if (!$params['toolDir']) 
+{ 
+  $toolsDir = $params['toolDir']
+}
+else 
+{
+  $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+}
 
 $file = "$($toolsDir)\docker.exe"
 
